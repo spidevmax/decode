d8b0e54c1337abe6724b3163cfcc0664c85b2288
Marina <marina.lsz522@gmail.com>
2025-08-16 18:16:44 +0200

    Modificado username Github

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => `
 <footer>
   <p>
     &copy; 2025, Decode. Powered by
-    <a href="https://github.com/marinalsz" rel="noopener" target="_blank">Marina López</a>
+    <a href="https://github.com/spidevmax" rel="noopener" target="_blank">spidevmax</a>
   </p>
 </footer>
 `;
